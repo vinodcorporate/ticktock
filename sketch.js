9a96560ef98bc4f6dcf1b108d838a7ee7aa87bb0
vinodcorporate <70857202+vinodcorporate@users.noreply.github.com>
2021-11-07 16:59:30 +0530

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,54 @@
+var hourhand;minutehand;secondshand
+var hr
+var mn
+var sc
+function setup() {
+  createCanvas(400,400);
+  angleMode(DEGREES)
+ 
+}
+
+function draw() {
+  background(255,255,255);
+  translate(200,200);
+  rotate(-90);
+  
+
+  hr=hour();
+  mn=minute();
+  sc=second();
+
+  secondshand=map(sc,0,60,0,360);
+  minutehand=map(mn,0,60,0,360);
+  hourhand=map(hr%12,0,12,0,360)
+  push()
+  rotate(secondshand)
+  stroke(255,0,0);
+  strokeWeight(7)
+  line(0,0,100,0);
+  pop();
+
+  push()
+  rotate(minutehand)
+  stroke(0,255,0);
+  strokeWeight(7)
+  line(0,0,75,0);
+  pop();
+
+  push()
+  rotate(hourhand)
+  stroke(0,0,255);
+  strokeWeight(7)
+  line(0,0,50,0);
+  pop();
+  stroke(255,0,255);
+  point(0,0)
+  strokeWeight(10)
+  noFill();
+  stroke(255,0,0)
+  arc(0,0,300,300,0,secondshand)
+  stroke(0,255,0)
+  arc(0,0,280,280,0,minutehand)
+  stroke(0,0,255)
+  arc(0,0,260,260,0,hourhand)
+}
